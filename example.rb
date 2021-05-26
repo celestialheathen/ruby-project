b15d4b200bd1e953e05460d21ad1154c5622caa8
celestialheathen <jingchaoyu@hotmail.com>
2021-05-26 13:56:55 -0400

    custom methods

--- example.rb
+++ example.rb
@@ -1,5 +1,15 @@
-fruits = %w[Apple Orange Grape Banana Starfruit Lime Lemon Apple]
+arr = [1, 3, 5, 7, 9, 15, 21, 18, 6]
 
 
-p fruits.empty?
-p fruits.nil?
+def custom_first(arr, num = 0)
+  return arr[0] if num == 0
+  arr[0, num]
+end 
+
+def custom_last(arr, num = 0)
+  return arr[-1] if num == 0
+  arr[-num..-1]
+end 
+
+p custom_first(arr, 4)
+p custom_last(arr, 5)
