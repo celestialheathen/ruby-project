6ea3e190ff916949f66df468c55beed629cfb6a2
celestialheathen <jingchaoyu@hotmail.com>
2021-05-26 14:39:39 -0400

    pop

--- example.rb
+++ example.rb
@@ -1,15 +1,7 @@
-arr = [1, 3, 5, 7, 9, 15, 21, 18, 6]
+locations = ["House", "Airport", "Bar"]
+p locations
 
+removed = locations.pop(4)
+p removed
 
-def custom_first(arr, num = 0)
-  return arr[0] if num == 0
-  arr[0, num]
-end 
-
-def custom_last(arr, num = 0)
-  return arr[-1] if num == 0
-  arr[-num..-1]
-end 
-
-p custom_first(arr, 4)
-p custom_last(arr, 5)
+p locations
