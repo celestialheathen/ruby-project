fbeef0435f910598d4389b2fe325e17ee193f08c
celestialheathen <jingchaoyu@hotmail.com>
2021-05-26 13:06:07 -0400

    count

--- example.rb
+++ example.rb
@@ -1,15 +1,6 @@
-fruits = %w[Apple Orange Grape Banana]
-p fruits
-
-fruits[4] = "Lemon"
-p fruits
+fruits = %w[Apple Orange Grape Banana Starfruit Lime Lemon Apple]
 
 
-fruits[10] = "Kiwi"
-p fruits
-
-fruits[3, 2] = ["Lime", "Mango"]
-p fruits
-
-fruits[0..3] = ["Strawberry"]
-p fruits
+p fruits.length
+p fruits.size
+p fruits.count("Apple")
