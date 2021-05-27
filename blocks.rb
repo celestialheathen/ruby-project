f2325affe9bc9c1ede702982c09548b81c2d492e
celestialheathen <jingchaoyu@hotmail.com>
2021-05-27 11:18:22 -0400

    each

---
+++ blocks.rb
@@ -0,0 +1,8 @@
+# 3.times { puts "Hello there." }
+
+# 3.times { |num| puts "I am currently on #{num}"}
+
+# 5.times do |i|
+#   square = i * i 
+#   puts "The current number is #{i} and its square is #{square}."
+# end 
--- example.rb
+++
@@ -1,3 +0,0 @@
-p [1, 2].is_a?(Array)
-p [1, 2].is_a?(Object)
-p [1, 2].is_a?(BasicObject)
