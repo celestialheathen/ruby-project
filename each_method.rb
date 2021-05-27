6c3e6963b9b442efa680598b440f1c74594920e9
celestialheathen <jingchaoyu@hotmail.com>
2021-05-27 11:34:41 -0400

    each

---
+++ each_method.rb
@@ -0,0 +1,8 @@
+candies = ["Chocolate stick", "Chips", "Cookies"]
+
+# candies.each do |candy|
+#   puts "I love eating #{candy}"
+#   puts "They taste so good!"
+# end 
+
+candies.each { |candy| puts candy.swapcase }
