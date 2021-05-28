ca5edebc64d6d9d2b00763a814e3747c83e4e614
celestialheathen <jingchaoyu@hotmail.com>
2021-05-28 15:24:36 -0400

    partition

---
+++ partition.rb
@@ -0,0 +1,11 @@
+foods = ["Steak", "Vegetables", "Steak Burger", "Kale", "Tofu", "Tuna Steak"]
+
+# good = foods.select { |food| food.include?("Steak") }
+# bad = foods.reject { |food| food.include?("Steak") }
+
+# p good 
+# p bad
+
+good, bad = foods.partition {|food| food.include?("Steak") }
+p good 
+p bad
