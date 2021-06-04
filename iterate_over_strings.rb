694918bb54cfcf7857de92a7b3cd3933237c8f08
celestialheathen <jingchaoyu@hotmail.com>
2021-06-04 14:17:37 -0400

    iteration

---
+++ iterate_over_strings.rb
@@ -0,0 +1,7 @@
+name = "james"
+
+# name.each_char { |c| puts c}
+
+# p name.split("")
+
+p name.chars
---
+++ split_method.rb
@@ -0,0 +1,5 @@
+sentence = "Hi, my name is James. There are spaces here!"
+
+# p sentence.split
+# p sentence.split(" ")
+p sentence.split(",")
