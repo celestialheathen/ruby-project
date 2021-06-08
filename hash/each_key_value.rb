c2f92efc01b2fc264d943456b0d4927cf956321c
celestialheathen <jingchaoyu@hotmail.com>
2021-06-08 16:58:49 -0400

    keys_values

--- hash/each_key_value.rb
+++ hash/each_key_value.rb
@@ -1,21 +1,29 @@
 salaries = {director: 100000, producer: 200000, ceo: 300000}
 
-def return_keys(hash) 
-  keys = []
-  hash.each do |pair|
-    keys << pair[0] 
-  end 
-  keys
+# def return_keys(hash) 
+#   keys = []
+#   hash.each do |pair|
+#     keys << pair[0] 
+#   end 
+#   keys
+# end 
+
+
+# def return_values(hash)
+#   values = []
+#   hash.each do |pair|
+#     values << pair[1]
+#   end 
+#   values
+# end 
+
+# p return_keys(salaries)
+# p return_values(salaries)
+
+salaries.each_key do |key|
+  puts key 
 end 
 
-
-def return_values(hash)
-  values = []
-  hash.each do |pair|
-    values << pair[1]
-  end 
-  values
+salaries.each_value do |value|
+  puts value 
 end 
-
-p return_keys(salaries)
-p return_values(salaries)
