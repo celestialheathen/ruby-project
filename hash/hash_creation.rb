603f6ff7fa6da96a72e84536e9dd8c7a7e82d28a
celestialheathen <jingchaoyu@hotmail.com>
2021-06-08 12:18:07 -0400

    hashes

---
+++ hash/hash_creation.rb
@@ -0,0 +1,10 @@
+roster = {"Jim" => "Student", "Chase" => "Coach", "Eric" => "Instructor"}
+
+cohorts = {"SE" => ["Jim", "Gabe", "Will"],
+           "DS" => ["Hussain", "Eric", "Poon"]}
+
+p roster
+p cohorts
+
+p roster["Jim"]
+p cohorts["SE"][1]
---
+++ hash/symbols.rb
@@ -0,0 +1,13 @@
+p :name 
+p :name.class  
+p :name.methods.length
+
+p "name"
+p "name".class
+p "name".methods.length
+
+# person = {:name => "James", :age => 38, :student => true}
+
+person = {name: "James Yu", age: 38, student: true}
+
+p person[:age]
