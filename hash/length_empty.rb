879b743b5477fcace68ed6bbd8be8b4ae932e745
celestialheathen <jingchaoyu@hotmail.com>
2021-06-08 14:50:05 -0400

    length

---
+++ hash/length_empty.rb
@@ -0,0 +1,8 @@
+shopping_list = {coffee: 4, sandwich: 1, cookies: 1}
+vegan_shopping_list = {}
+
+p shopping_list.length
+p vegan_shopping_list.length
+
+p shopping_list.empty?
+p vegan_shopping_list.empty?
