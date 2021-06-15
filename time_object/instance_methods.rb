546833263afb18e87a490b745d9aff817e47582a
celestialheathen <jingchaoyu@hotmail.com>
2021-06-15 12:48:15 -0400

    time object

---
+++ time_object/instance_methods.rb
@@ -0,0 +1,15 @@
+today = Time.now
+
+p today.class
+p today.year
+p today.month
+p today.day
+
+p today.hour
+p today.min 
+p today.sec
+
+puts
+
+p today.yday
+p today.wday
---
+++ time_object/intro_to_time.rb
@@ -0,0 +1,6 @@
+# puts Time.new 
+# puts Time.now
+
+puts Time.new(2050, 12, 30)
+
+puts Time.new(3000, 1, 1, 12, 30, 55)
