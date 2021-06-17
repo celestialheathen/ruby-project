d53a3985da31d606046c818b75f8f29bd1bffe0c
celestialheathen <jingchaoyu@hotmail.com>
2021-06-17 14:21:19 -0400

    input and output

---
+++ file_io/write_file.rb
@@ -0,0 +1,9 @@
+# File.open("a_new_text_file_txt", "w") do |file|
+#   file.puts "I am starting my JSSD"
+#   file.write "What kind of job are you looking for?"
+# end 
+
+File.open("text.txt", "a") do |file|
+  file.puts 
+  file.puts "Trying it out with full path."
+end 
