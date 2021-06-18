130297f403350ea0fbe73986baf010a8f352da66
celestialheathen <jingchaoyu@hotmail.com>
2021-06-18 14:29:17 -0400

    re

---
+++ regular_expression/start_end_with.rb
@@ -0,0 +1,15 @@
+phrase = "My Japanese trip will be sometime next year hopefully."
+
+
+def custom_start_with?(string, substring)
+  string.slice(0, substring.length) == substring ? true : false
+end 
+
+
+def custom_end_with?(string, substring)
+  string.slice((substring.length * -1)..-1) == substring ? true : false
+end 
+
+
+p custom_start_with?(phrase, "My J")
+p custom_end_with?(phrase, "y.")
