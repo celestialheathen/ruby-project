25359bca6139105a17dae76b34f692029ccf9af5
celestialheathen <jingchaoyu@hotmail.com>
2021-06-19 16:00:22 -0400

    regex

---
+++ regular_expression/anchors.rb
@@ -0,0 +1,8 @@
+poem = "It was many and many a year ago,
+In a kingdom by the sea,
+That a maiden there lived It whom you may know
+By the name of Annabel Lee;
+And this maiden she It lived with no other thought
+Than to love and be loved by me"
+
+p poem.scan(/me\z/)
