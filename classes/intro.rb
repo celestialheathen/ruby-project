f0c3160c89de3cc14e2f7f069ac8371eab25aa95
celestialheathen <jingchaoyu@hotmail.com>
2021-06-21 12:09:27 -0400

    classes

---
+++ classes/intro.rb
@@ -0,0 +1,18 @@
+puts 5.class  
+puts 5.class.superclass
+puts 5.class.superclass.superclass
+puts 5.class.superclass.superclass.superclass
+
+puts
+
+puts 3.14.class
+puts 3.14.class.superclass
+puts 3.14.class.superclass.superclass
+
+puts
+
+puts Float.ancestors
+
+puts 
+
+puts TrueClass.ancestors
