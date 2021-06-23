bca240d86b9307ed9f08b8c35bc371540592ff32
celestialheathen <jingchaoyu@hotmail.com>
2021-06-23 14:08:29 -0400

    polymorph

--- classes/instance_variables.rb
+++ classes/instance_variables.rb
@@ -4,10 +4,14 @@ class Gadget
     @password = "12345"
     @serial = "#{rand(1..1000)}"
   end 
+
+  def to_s 
+    "Gadget #{@serial} belongs to the user #{@username}"
+  end 
+
 end 
 
 phone = Gadget.new
 laptop = Gadget.new
 
-p phone.instance_variables
-p laptop.instance_variables
+
